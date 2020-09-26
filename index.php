ae27df73dd24e3da4f769d73fa6047780e528105
arturo9999a <56418813+arturo9999a@users.noreply.github.com>
2020-09-25 19:35:33 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,173 @@
+﻿<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Viaja seguro y con estilo</title>
+    <!-- Description, Keywords and Author -->
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your,Keywords">
+    <meta name="author" content="ResponsiveWebInc">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Styles -->
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font awesome CSS -->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="#">
+</head>
+
+<body>
+
+    <!-- wrapper -->
+    <div class="wrapper">
+        <!-- banner -->
+        <div class="banner">
+            <div class="container">
+                <!-- banner content -->
+                <div class="banner-content">
+                    <!-- logo image 
+					<a href="#"><img class="img-responsive" src="img/logo.png" alt="" /></a>-->
+                    <p>
+                        Viaje seguro
+                    </p>
+                    <!-- short paragraph -->
+                    <p>Contrata nuestro servicio de chofer<br>Nuestro servicio es el mejor</p>
+                    <!-- button link -->
+                    <a class="btn btn-info btn-lg" target="_blanck" href="http://api.whatsapp.com/send?phone=+529991297681&amp;text=hola!&amp;quisiera&amp;mas&amp;inf." class="btn btn-info btn-lg">Contratar ahora</a>
+
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+
+        <!-- feature block -->
+        <div class="feature">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-4">
+                        <!-- feature item -->
+                        <div class="feature-item">
+                            <!-- icon -->
+                            <a href="#"><img class="img-responsive" src="img/1.jpg" alt="" /></a>
+                            <!-- heading -->
+                            <h4>Haz que tu estilo brille</h4>
+                            <!-- paragraph -->
+                            <p>Con sus faros de niebla* y luces de marcha diurna (DRL LED).</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <!-- feature item -->
+                        <div class="feature-item">
+                            <!-- icon -->
+                            <a href="#"><img class="img-responsive" src="img/2.jpg" alt="" /></a>
+                            <!-- heading -->
+                            <h4> Obtén un look imponente</h4>
+                            <!-- paragraph -->
+                            <p>Con rines de aluminio de 16”*, defensa delantera con spoiler y parrilla cromada</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <!-- feature item -->
+                        <div class="feature-item">
+                            <!-- icon -->
+                            <a href="#"><img class="img-responsive" src="img/descarga.jpg" alt="" /></a>
+                            <!-- heading -->
+                            <h4>Conquista el camino con estilo</h4>
+                            <!-- paragraph -->
+                            <p>Aduéñate de todas las miradas con Honda City y disfruta su deportividad gracias a su exclusivo y audaz diseño.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <!-- feature block -->
+        <div class="feature">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-4">
+                        <!-- feature item -->
+                        <div class="feature-item">
+                            <!-- icon -->
+                            <a href="#"><img class="img-responsive" src="img/1 (1).jpg" alt="" /></a>
+                            <!-- heading -->
+                            <h4>Más diversión, mejor confort</h4>
+                            <!-- paragraph -->
+                            <p>Un espacio a tu medida Honda City ofrece una cabina confortable para todos los pasajeros. Encontrarás en su amplio espacio interior, muchos detalles que simplifican tu vida como una gran consola central con espacio para almacenaje
+                                y descansabrazos.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <!-- feature item -->
+                        <div class="feature-item">
+                            <!-- icon -->
+                            <a href="#"><img class="img-responsive" src="img/2 (1).jpg" alt="" /></a>
+                            <!-- heading -->
+                            <h4> Asientos traseros abatibles</h4>
+                            <!-- paragraph -->
+                            <p>Honda City cuenta con amplia cajuela y asientos traseros abatibles* para llevar todo lo que quieras, incluso objetos grandes y voluminosos. Además, el asiento del conductor cuenta con ajuste manual de 6 vías.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <!-- feature item -->
+                        <div class="feature-item">
+                            <!-- icon -->
+                            <a href="#"><img class="img-responsive" src="img/3 (1).jpg" alt="" /></a>
+                            <!-- heading -->
+                            <h4>Aire acondicionado con panel de control touchscreen*</h4>
+                            <!-- paragraph -->
+                            <p>Con su sistema de aire acondicionado con panel de control touch screen* de clima automático*, maneja la temperatura de tu Honda City de forma sencilla.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+        <!-- footer -->
+        <footer>
+            <div class="container">
+                <!-- navigation link 
+                <p class="nav-link"><a href="#">Contact</a> &nbsp;|&nbsp; <a href="#">Downloads</a> &nbsp;|&nbsp; <a href="#">Email</a> &nbsp;|&nbsp; <a href="#">Support</a> &nbsp;|&nbsp; <a href="#">Privacy Policy</a></p>-->
+                <!-- copy right -->
+                <!-- This theme comes under Creative Commons Attribution 4.0 Unported. So don't remove below link back -->
+                <p class="copy-right">Copyright &copy; <a href="#">Your Site</a> . </p>
+            </div>
+        </footer>
+
+    </div>
+
+
+    <!-- Javascript files -->
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Respond JS for IE8 -->
+    <script src="js/respond.min.js"></script>
+    <!-- HTML5 Support for IE -->
+    <script src="js/html5shiv.js"></script>
+    <!-- Custom JS -->
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
